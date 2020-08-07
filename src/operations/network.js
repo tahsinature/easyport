17662887ac4a9f9486793fb684809a5128a6d34c
Tahsin <tahsingenuies@gmail.com>
2020-08-07 19:42:15 +0700

    feat: 🎸 add ip tools

--- src/operations/network.js
+++ src/operations/network.js
@@ -1,4 +1,9 @@
 const { Command } = require("@oclif/command");
+const internalIp = require("internal-ip");
+const publicIp = require("public-ip");
+
+internalIp.v4().then(console.log);
+publicIp.v4().then(console.log);
 
 // get your ip details
 // ssh
